6eab0299bceb61149ac2a27db0b6f44876127bbb
Naoshin Anzum Hridi <naoshinhridi@gmail.com>
2024-01-23 23:00:44 +0600

    start authorizing the automation

---
+++ src/test/java/config/TestUserConfig.java
@@ -0,0 +1,20 @@
+package config;
+
+import aquality.selenium.core.utilities.ISettingsFile;
+import aquality.selenium.core.utilities.JsonSettingsFile;
+
+public class TestUserConfig {
+
+    private static final ISettingsFile SETTINGS_FILE = new JsonSettingsFile("testUser.json");
+    public static String getUserPath() {
+        return SETTINGS_FILE.getValue("/login").toString();
+    }
+
+    public static String getPassword() {
+        return SETTINGS_FILE.getValue("/password").toString();
+    }
+
+    public static String getToken() {
+        return SETTINGS_FILE.getValue("/token").toString();
+    }
+}
---
+++ src/test/java/utils/BrowserUtils.java
@@ -0,0 +1,25 @@
+package utils;
+
+import aquality.selenium.browser.AqualityServices;
+import constant.CommonConstants;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BrowserUtils {
+
+    public static void waitForPageLoad(String url, By pageLoadIndicator) {
+        WebDriver driver = AqualityServices.getBrowser().getDriver();
+        driver.get(url);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(CommonConstants.COMMON_WAIT_TIME));
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(pageLoadIndicator));
+        } catch (Exception e) {
+            throw new RuntimeException("Page did not load properly or the element was not found: " + e.getMessage());
+        }
+    }
+}
